594f248992d849c49f88f3290556259b3c046dd9
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-04-14 17:06:43 -0400

    Update Image.py

--- modules/Image.py
+++ modules/Image.py
@@ -11,19 +11,6 @@ class Image(commands.Cog, name="Image"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(name='danbooru', help='Search for an image on Danbooru.')
-    async def danbooru_search(self, ctx, *tags):
-        if not tags:
-            await ctx.send('Please specify some tags to search for.')
-            return
-        async with aiohttp.ClientSession() as session:
-            results = await self.danbooru.post_list(tags=tags, limit=50, session=session)
-        if not results:
-            await ctx.send('No results found.')
-            return
-        result = random.choice(results)
-        await ctx.send(result['file_url'])
-
     @commands.command()
     async def neko(self, ctx):
         if ctx.message.channel.is_nsfw():

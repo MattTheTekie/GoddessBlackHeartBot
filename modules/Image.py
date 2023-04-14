9fd67648570283371663d0bbe097cdb45a6f81f4
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-04-14 16:50:06 -0400

    Update Image.py

--- modules/Image.py
+++ modules/Image.py
@@ -41,37 +41,5 @@ class Image(commands.Cog, name="Image"):
         embed.set_image(url=image['url'])
         await ctx.send(embed=embed)
 
-    @commands.command(name="danbooru")
-    async def danbooru(self, ctx, *, tags):
-        # Define API endpoint and request parameters
-        api_url = "https://danbooru.donmai.us/posts.json"
-        params = {
-            "limit": 1,
-            "tags": tags
-        }
-        
-        # Send GET request to Danbooru API
-        response = requests.get(api_url, params=params)
-        
-        # Check if response was successful
-        if response.status_code == 200:
-            # Parse JSON response
-            data = json.loads(response.text)
-            
-            # Check if any posts were found
-            if len(data) > 0:
-                # Extract image URL from post data
-                image_url = "https://danbooru.donmai.us" + data[0]["file_url"]
-                
-                # Send image to Discord server
-                embed = discord.Embed()
-                embed.set_image(url=image_url)
-                await ctx.send(embed=embed)
-            else:
-                await ctx.send("No posts found with those tags.")
-        else:
-            await ctx.send("An error occurred while communicating with the Danbooru API.")
-
 def setup(bot):
     bot.add_cog(Image(bot))
-    bot.add_cog(Danbooru(bot))

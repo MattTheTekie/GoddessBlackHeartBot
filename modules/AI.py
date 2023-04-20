030c0145bf275ce76b877f3e26733263acb9242b
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-04-20 19:39:38 -0400

    Add files via upload

--- modules/AI.py
+++ modules/AI.py
@@ -11,37 +11,37 @@ import subprocess
 import openai
 
 class AI(commands.Cog, name="AI"):
-
     def __init__(self, bot):
         self.bot = bot
         self.openai_token = "pk-lqRPVysXvAPeooisGFSZkNLzVGamczCHbarsOnAoEVzlhpPt"
 
     @commands.command()
-    async def ai(self, ctx, *, prompt:str):
-        headers = {
-            'Authorization': f'Bearer {self.openai_token}',
-            'Content-Type': 'application/json'
-        }
-        data = {
-            'model': 'text-davinci-002',
-            'prompt': f'"{prompt}"\nAI:',
-            'temperature': 0.7,
-            'max_tokens': 256,
-            'stop': ['Human:', 'AI:']
-        }
-        async with aiohttp.ClientSession() as session:
-            user_input = await self.get_user_input(ctx)
-            data['prompt'] = f'{prompt}\nUser: {user_input}\nAI:'
-            async with session.post('https://api.pawan.krd/v1/completions', headers=headers, json=data) as response:
-                if response.status == 200:
-                    result = await response.json()
-                    text = result['choices'][0]['text']
-                    await ctx.send(f"```\n{text}\n```")
-                else:
-                    await ctx.send(f"Command failed with status code {response.status}")
+    async def ai(self, ctx, *, prompt: str):
+        async with ctx.typing():
+            headers = {
+                'Authorization': f'Bearer {self.openai_token}',
+                'Content-Type': 'application/json'
+            }
+            data = {
+                'model': 'text-davinci-002',
+                'prompt': f'"{prompt}"\nAI:',
+                'temperature': 0.7,
+                'max_tokens': 256,
+                'stop': ['Human:', 'AI:']
+            }
+
+            async with aiohttp.ClientSession() as session:
+                user_input = await self.get_user_input(ctx)
+                data['prompt'] = f'{prompt}\nUser: {user_input}\nAI:'
+                async with session.post('https://api.pawan.krd/v1/completions', headers=headers, json=data) as response:
+                    if response.status == 200:
+                        result = await response.json()
+                        text = result['choices'][0]['text']
+                        await ctx.send(f"```\n{text}\n```")
+                    else:
+                        await ctx.send(f"Command failed with status code {response.status}")
 
     async def get_user_input(self, ctx):
-        # message = await ctx.send("Please provide your input:")
         def check(m):
             return m.author == ctx.author and m.channel == ctx.channel
         user_input_msg = await self.bot.wait_for('message', check=check)

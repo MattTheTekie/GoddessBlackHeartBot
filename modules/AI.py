0617214bb963a0c70829a1f6e076437bda4e46ec
AugustusXVIII <61804916+Austcool-Walker@users.noreply.github.com>
2023-04-21 11:39:57 -0400

    Fixed AI for use with our chatgpt proxy.

--- modules/AI.py
+++ modules/AI.py
@@ -1,26 +1,32 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import json
+from datetime import date, time, datetime, timedelta
+import aiohttp
+import random
 import requests
+import asyncio
+import subprocess
 
-class AI(commands.Cog):
+class AI(commands.Cog, name="AI"):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def ai(self, ctx, *, prompt: str):
-        url = 'http://127.0.0.1:8080/api'
-        headers = {'Content-Type': 'application/json'}
-        data = {
-            'model': 'openai:gpt-3.5-turbo',
-            'prompt': prompt
-        }
-        response = requests.post(url, headers=headers, json=data)
-        response_dict = response.json()
-        text = response_dict.get('text')
-        if text:
-            await ctx.send(text)
-        else:
-            await ctx.send("Sorry, I couldn't generate a response.")
+    @commands.command()        
+    async def ai(self, ctx, *, prompt):
+        cmd = f'''curl --silent --location --request POST 'http://127.0.0.1:8080/api' \
+--header 'Content-Type: application/json' \
+--data-raw '{{
+ 
+        "model": "openai:gpt-3.5-turbo",
+        "prompt": "{prompt}"
+}}' | grep "text"'''
+        try:
+            result = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+            await ctx.send(f"```\n{result}\n```")
+        except subprocess.CalledProcessError as exc:
+            await ctx.send(f"Command failed with exit code {exc.returncode}: ```\n{exc.output}\n```")
 
 def setup(bot):
     bot.add_cog(AI(bot))

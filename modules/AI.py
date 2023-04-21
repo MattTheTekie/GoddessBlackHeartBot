bbd1c8db42bff20a4f855bcc817d350ad3355309
AugustusXVIII <61804916+Austcool-Walker@users.noreply.github.com>
2023-04-21 15:48:42 -0400

    Update AI.py

--- modules/AI.py
+++ modules/AI.py
@@ -8,35 +8,39 @@ class AI(commands.Cog, name="AI"):
         self.anime_character = None
 
     @commands.command()
-    async def setch(self, ctx, *, anime_character=None):
-        if anime_character is not None:
-            self.anime_character = " ".join(anime_character.split())
+    async def setch(self, ctx, anime_character: str = None):
+        if anime_character:
+            self.anime_character = anime_character.strip()
+            await ctx.send(f"AI model set to: {self.anime_character}")
+        else:
+            await ctx.send("Session state of the AI model has been reset.")
+            self.anime_character = None
+        await ctx.message.add_reaction('👌')
+
+    @commands.command()        
+    async def ai(self, ctx, *, prompt):
+        if prompt.lower().startswith("setch "):
+            self.anime_character = prompt[14:]
             await ctx.send(f"AI model set to: {self.anime_character}")
             await ctx.message.add_reaction('👌')
         else:
-            await ctx.send("Session state of the AI model has been reset.")
-            await ctx.message.add_reaction('👌')
-            self.anime_character = None
-
-    @commands.command()
-    async def ai(self, ctx, *, prompt):
-        data = {
-            "model": self.get_ai_model(),
-            "prompt": prompt,
-        }
-        # Send typing indicator to indicate bot is processing request
-        async with ctx.typing():
-            try:
-                async with aiohttp.ClientSession() as session:
-                    async with session.post('http://127.0.0.1:8080/api', json=data) as response:
-                        result = await response.json()
-                        await ctx.send(f"```\n{result['data']}\n```")
-            except Exception as e:
-                await ctx.send(f"An error occurred while processing your request: {e}")
+            data = {
+                "model": self.get_ai_model(),
+                "prompt": prompt,
+            }
+            # Send typing indicator to indicate bot is processing request
+            async with ctx.typing():
+                try:
+                    async with aiohttp.ClientSession() as session:
+                        async with session.post('http://127.0.0.1:8080/api', json=data) as response:
+                            result = await response.json()
+                            await ctx.send(f"```\n{result['data']}\n```")
+                except Exception as e:
+                    await ctx.send(f"An error occurred while processing your request: {e}")
 
     def get_ai_model(self):
         if self.anime_character:
-            return f"openai:text-davinci-003, persona={self.anime_character}"
+            return "openai:text-davinci-003"
         else:
             return "openai:text-davinci-002"
 

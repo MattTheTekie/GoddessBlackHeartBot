e10f6586cb314515daf11a1e90f66b94deee88fe
AugustusXVIII <61804916+Austcool-Walker@users.noreply.github.com>
2023-04-21 15:25:59 -0400

    Tried to make the reset session work again..... 🤞

--- modules/AI.py
+++ modules/AI.py
@@ -6,50 +6,39 @@ class AI(commands.Cog, name="AI"):
     def __init__(self, bot):
         self.bot = bot
         self.anime_character = None
-        self.default_model = "openai:gpt-3.5-turbo"
 
-    @commands.command()        
-    async def ai(self, ctx, *, prompt):
-        if prompt.lower().startswith("setch "):
-            character = prompt[6:].strip()
-            if character.lower() == "default":
-                self.anime_character = None
-                await ctx.send("Character set to default.")
-                await self.update_bot_profile()
-            else:
-                self.anime_character = character
-                await ctx.send(f"Character set to: {self.anime_character}")
-                await self.update_bot_profile()
+    @commands.command()
+    async def setch(self, ctx, *, anime_character=None):
+        if anime_character:
+            self.anime_character = anime_character
+            await ctx.send(f"AI model set to: {self.anime_character}")
+            await ctx.message.add_reaction('👌')
         else:
-            data = {
-                "model": self.get_ai_model(),
-                "prompt": prompt
-            }
-            async with ctx.typing():
-                try:
-                    async with aiohttp.ClientSession() as session:
-                        async with session.post('http://127.0.0.1:8080/api', json=data) as response:
-                            result = await response.json()
-                            await ctx.send(f"```\n{result['data']}\n```")
-                except Exception as e:
-                    await ctx.send(f"An error occurred while processing your request: {e}")
+            self.anime_character = None
+            await ctx.send("Session state of the AI model has been reset.")
+            await ctx.message.add_reaction('👌')
+
+    @commands.command()
+    async def ai(self, ctx, *, prompt):
+        data = {
+            "model": self.get_ai_model(),
+            "prompt": prompt,
+        }
+        # Send typing indicator to indicate bot is processing request
+        async with ctx.typing():
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.post('http://127.0.0.1:8080/api', json=data) as response:
+                        result = await response.json()
+                        await ctx.send(f"```\n{result['data']}\n```")
+            except Exception as e:
+                await ctx.send(f"An error occurred while processing your request: {e}")
 
     def get_ai_model(self):
         if self.anime_character:
-            return f"openai:text-davinci-003-{self.anime_character.lower().replace(' ', '-')}"
+            return "openai:text-davinci-003"
         else:
-            return self.default_model
-
-    async def update_bot_profile(self):
-        if self.anime_character:
-            username = self.anime_character
-        else:
-            username = "My AI Bot"
-        await self.bot.user.edit(username=username)
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        await self.update_bot_profile()
+            return "openai:text-davinci-002"
 
 def setup(bot):
     bot.add_cog(AI(bot))

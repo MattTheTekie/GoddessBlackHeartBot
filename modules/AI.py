0f97fb8f5661ec86dcbad21b27f3be304a6bac71
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-04-21 14:31:39 -0400

    Update AI.py

--- modules/AI.py
+++ modules/AI.py
@@ -1,3 +1,4 @@
+
 import discord
 from discord.ext import commands
 import aiohttp
@@ -6,61 +7,40 @@ class AI(commands.Cog, name="AI"):
     def __init__(self, bot):
         self.bot = bot
         self.anime_character = None
-        self.load_config()
 
-    async def save_config(self):
-        with open('config.txt', 'w') as f:
-            f.write(self.anime_character or "default")
-
-    def load_config(self):
-        try:
-            with open('config.txt', 'r') as f:
-                anime_character = f.read().strip()
-                if anime_character.lower() == "default":
-                    self.anime_character = None
-                else:
-                    self.anime_character = anime_character
-        except FileNotFoundError:
-            pass
-    
-    @commands.command()
-    async def setch(self, ctx, *, anime_character):
-        if anime_character.lower() == "default":
-            self.anime_character = None
-            await ctx.send("AI model reset to default")
-        else:
-            self.anime_character = anime_character
-            await ctx.send(f"AI model set to: {self.anime_character}")
-        
-        await self.save_config()
-        await ctx.message.add_reaction('👌')
-    
-    @commands.command()
+    @commands.command()        
     async def ai(self, ctx, *, prompt):
-        data = {
-            "model": self.get_ai_model(),
-            "prompt": prompt,
-        }
-        # Send typing indicator to indicate bot is processing request
-        async with ctx.typing():
-            try:
-                async with aiohttp.ClientSession() as session:
-                    async with session.post('http://127.0.0.1:8080/api', json=data) as response:
-                        result = await response.json()
-                        response_chunks = [result['data'][i:i+2000] for i in range(0, len(result['data']), 2000)]
-                        for chunk in response_chunks:
-                            await ctx.send(f"```\n{chunk}\n```")
-            except discord.errors.HTTPException as e:
-                # Split the response into chunks of 2000 characters or less
-                response_chunks = [result['data'][i:i+1900] for i in range(0, len(result['data']), 1900)]
-                for chunk in response_chunks:
-                    await ctx.send(f"```\n{chunk}\n```")
+        if prompt.lower().startswith("setch "):
+            if prompt[14:].lower() == "default":
+                self.anime_character = None
+                await ctx.send("AI model reset to default")
+                await ctx.message.add_reaction('👌')
+            else:
+                self.anime_character = prompt[14:]
+                await ctx.send(f"AI model set to: {self.anime_character}")
+                await ctx.message.add_reaction('👌')
+        else:
+            data = {
+                "model": self.get_ai_model(),
+                "prompt": prompt,
+            }
+            # Send typing indicator to indicate bot is processing request
+            async with ctx.typing():
+                try:
+                    async with aiohttp.ClientSession() as session:
+                        async with session.post('http://127.0.0.1:8080/api', json=data) as response:
+                            result = await response.json()
+                            response_chunks = [result['data'][i:i+2000] for i in range(0, len(result['data']), 2000)]
+                            for chunk in response_chunks:
+                                await ctx.send(f"```\n{chunk}\n```")
+                except Exception as e:
+                    await ctx.send(f"An error occurred while processing your request: {e}")
 
     def get_ai_model(self):
         if self.anime_character:
-            return "openai/text-davinci-003"
+            return "openai:text-davinci-003"
         else:
-            return "openai:gpt-3.5-turbo"
+            return "openai:text-davinci-002"
 
 def setup(bot):
     bot.add_cog(AI(bot))

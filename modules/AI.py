c76538e989f6dde9dc1d4b3491b87ecb111ca777
AugustusXVIII <61804916+Austcool-Walker@users.noreply.github.com>
2023-04-21 13:06:40 -0400

    Hopefully properly integrated bot preflight when processing requests? (The

--- modules/AI.py
+++ modules/AI.py
@@ -1,7 +1,6 @@
 import discord
 from discord.ext import commands
 import aiohttp
-import subprocess
 
 class AI(commands.Cog, name="AI"):
     def __init__(self, bot):
@@ -13,13 +12,15 @@ class AI(commands.Cog, name="AI"):
             "model": "openai:gpt-3.5-turbo",
             "prompt": prompt
         }
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.post('http://127.0.0.1:8080/api', json=data) as response:
-                    result = await response.json()
-                    await ctx.send(f"```\n{result['data']}\n```")
-        except Exception as e:
-            await ctx.send(f"An error occurred while processing your request: {e}")
+        # Send typing indicator to indicate bot is processing request
+        async with ctx.typing():
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.post('http://127.0.0.1:8080/api', json=data) as response:
+                        result = await response.json()
+                        await ctx.send(f"```\n{result['data']}\n```")
+            except Exception as e:
+                await ctx.send(f"An error occurred while processing your request: {e}")
 
 def setup(bot):
     bot.add_cog(AI(bot))

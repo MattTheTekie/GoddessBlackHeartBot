9adeb5fc440a638dd4403ae43e4d3648d542f8c4
AugustusXVIII <61804916+Austcool-Walker@users.noreply.github.com>
2023-04-21 12:21:21 -0400

    Still trying to get ai command to respond with answer the question.

--- modules/AI.py
+++ modules/AI.py
@@ -7,15 +7,16 @@ import asyncio
 import subprocess
 
 class AI(commands.Cog, name="AI"):
+
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def ai(self, ctx, *, prompt):
+    async def ai(self, ctx):
         url = 'http://127.0.0.1:8080/api'
         data = {
             "model": "openai:gpt-3.5-turbo",
-            "prompt": prompt
+            "prompt": "What is Ubuntu?"
         }
         headers = {
             'Content-Type': 'application/json'

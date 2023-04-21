f670985be7d94b32356b4b197beb0ff4b756d2e4
AugustusXVIII <61804916+Austcool-Walker@users.noreply.github.com>
2023-04-21 12:13:53 -0400

    once again trying to get a proper responce from the AI proxy.

--- modules/AI.py
+++ modules/AI.py
@@ -1,47 +1,31 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands.cooldowns import BucketType
 import json
-from datetime import date, time, datetime, timedelta
 import aiohttp
 import random
-import requests
 import asyncio
 import subprocess
-import openai
 
-class AI(commands.Cog):
+class AI(commands.Cog, name="AI"):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def ai(self, ctx):
-        await ctx.send("What would you like to say to the AI?")
-
-        def check(message):
-            return message.author == ctx.author and message.channel == ctx.channel
-
-        user_input = await self.bot.wait_for('message', check=check)
-
+        url = 'http://127.0.0.1:8080/api'
         data = {
-            'model': 'openai:gpt-3.5-turbo',
-            'prompt': f'{user_input.content}',
-            'temperature': 0.7,
-            'max_tokens': 256,
-            'stop': ['Human:', 'AI:']
+            "model": "openai:gpt-3.5-turbo",
+            "prompt": "您是一个Rust语言专家,我有问题需要问你。\n\n请问如何写一个hello world程序?"
+        }
+        headers = {
+            'Content-Type': 'application/json'
         }
 
         async with aiohttp.ClientSession() as session:
-            async with session.post('http://127.0.0.1:8080/api', json=data) as response:
-                if response.status == 200:
-                    result = await response.json()
-                    if 'choices' in result:
-                        text = result['choices'][0]['text']
-                        await ctx.send(f"AI: {text}")
-                    else:
-                        await ctx.send("Sorry, I didn't get a response from the AI.")
-                else:
-                    await ctx.send(f"Command failed with status code {response.status}")
-
+            async with session.post(url, headers=headers, data=json.dumps(data)) as response:
+                result = await response.json()
+                text_result = result.get("text", "")
+                await ctx.send(f"```{text_result}```")
+        
 def setup(bot):
     bot.add_cog(AI(bot))

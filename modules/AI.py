781b10e6d3899b810dead0ebea23a1005f1bbe80
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-04-21 10:13:01 -0400

    Update AI.py

--- modules/AI.py
+++ modules/AI.py
@@ -1,34 +1,21 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands.cooldowns import BucketType
-import json
-from datetime import date, time, datetime, timedelta
-import aiohttp
-import random
 import requests
-import asyncio
-import subprocess
-import openai
 
-client = discord.Client()
+class AI(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-client = commands.Bot(command_prefix='!')
-
-@client.command()
-async def ai(ctx, *, prompt: str):
-    url = 'http://100.110.158.36:8080/api'
-    headers = {'Content-Type': 'application/json'}
-    data = {
-        'model': 'openai:gpt-3.5-turbo',
-        'prompt': prompt
-    }
-    response = requests.post(url, headers=headers, json=data)
-    await ctx.send(response.json()['text'])
+    @commands.command()
+    async def ai(self, ctx, *, prompt: str):
+        url = 'http://100.110.158.36:8080/api'
+        headers = {'Content-Type': 'application/json'}
+        data = {
+            'model': 'openai:gpt-3.5-turbo',
+            'prompt': prompt
+        }
+        response = requests.post(url, headers=headers, json=data)
+        await ctx.send(response.json()['text'])
 
 def setup(bot):
     bot.add_cog(AI(bot))

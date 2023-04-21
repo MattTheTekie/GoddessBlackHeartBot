13a23a38219b495f6c6af3ee94872374a53d28c2
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-04-21 12:06:53 -0400

    Update AI.py

--- modules/AI.py
+++ modules/AI.py
@@ -1,6 +1,14 @@
 import discord
 from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import json
+from datetime import date, time, datetime, timedelta
 import aiohttp
+import random
+import requests
+import asyncio
+import subprocess
+import openai
 
 class AI(commands.Cog):
     def __init__(self, bot):
@@ -8,36 +16,32 @@ class AI(commands.Cog):
 
     @commands.command()
     async def ai(self, ctx):
-        await ctx.send("Please enter your prompt:")
-        prompt = await self.get_user_input(ctx)
+        await ctx.send("What would you like to say to the AI?")
 
-        headers = {
-            'Content-Type': 'application/json'
-        }
+        def check(message):
+            return message.author == ctx.author and message.channel == ctx.channel
+
+        user_input = await self.bot.wait_for('message', check=check)
 
         data = {
             'model': 'openai:gpt-3.5-turbo',
-            'prompt': f'Human: {prompt}\nAI:',
+            'prompt': f'Human: {user_input.content}\nAI:',
             'temperature': 0.7,
             'max_tokens': 256,
             'stop': ['Human:', 'AI:']
         }
 
         async with aiohttp.ClientSession() as session:
-            async with ctx.typing():
-                async with session.post('http://127.0.0.1:8080/api', headers=headers, json=data) as response:
-                    if response.status == 200:
-                        result = await response.json()
+            async with session.post('http://127.0.0.1:8080/api', json=data) as response:
+                if response.status == 200:
+                    result = await response.json()
+                    if 'choices' in result:
                         text = result['choices'][0]['text']
-                        await ctx.send(f"```\n{text}\n```")
+                        await ctx.send(f"AI: {text}")
                     else:
-                        await ctx.send(f"Command failed with status code {response.status}")
-
-    async def get_user_input(self, ctx):
-        def check(m):
-            return m.author == ctx.author and m.channel == ctx.channel
-        user_input_msg = await self.bot.wait_for('message', check=check)
-        return user_input_msg.content
+                        await ctx.send("Sorry, I didn't get a response from the AI.")
+                else:
+                    await ctx.send(f"Command failed with status code {response.status}")
 
 def setup(bot):
     bot.add_cog(AI(bot))

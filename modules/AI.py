caea2c69f029bf9e18b576a266093bd3c419d81c
AugustusXVIII <61804916+Austcool-Walker@users.noreply.github.com>
2023-04-21 11:48:50 -0400

    Hopefully fixed the AI cog preflight error.

--- modules/AI.py
+++ modules/AI.py
@@ -12,20 +12,8 @@ class AI(commands.Cog, name="AI"):
     def __init__(self, bot):
         self.bot = bot
 
-    async def preflight(self, ctx, prompt):
-        async with aiohttp.ClientSession() as session:
-            async with session.options('http://127.0.0.1:8080/api', headers={'Content-Type': 'application/json', 'Access-Control-Request-Method': 'POST', 'Access-Control-Request-Headers': 'Content-Type'}) as resp:
-                if resp.status != 200:
-                    await ctx.send(f"Preflight failed with status {resp.status}")
-                    return False
-                else:
-                    return True
-
     @commands.command()
     async def ai(self, ctx, *, prompt):
-        if not await self.preflight(ctx, prompt):
-            return
-
         async with aiohttp.ClientSession() as session:
             data = {
                 "model": "openai:gpt-3.5-turbo",

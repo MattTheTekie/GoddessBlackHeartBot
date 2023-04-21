d9a6fc2d10357157b46d569abf4eeb96e13ca125
MattTheTekie <94870997+MattTheTekie@users.noreply.github.com>
2023-04-21 11:52:43 -0400

    Create AI.py

---
+++ modules/AI.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import json
+from datetime import date, time, datetime, timedelta
+import aiohttp
+import random
+import requests
+import asyncio
+
+class AI(commands.Cog, name="AI"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def ai(self, ctx, *, prompt):
+        async with aiohttp.ClientSession() as session:
+            data = {
+                "model": "openai:gpt-3.5-turbo",
+                "prompt": prompt
+            }
+            async with session.post('http://127.0.0.1:8080/api', json=data) as resp:
+                if resp.status == 200:
+                    result = await resp.json()
+                    await ctx.send(f"```\n{result['text']}\n```")
+                else:
+                    await ctx.send(f"Command failed with status {resp.status}")
+
+def setup(bot):
+    bot.add_cog(AI(bot))

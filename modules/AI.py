cc42cdfe127cb73101faa76b93a93a81a077685d
AugustusXVIII <61804916+Austcool-Walker@users.noreply.github.com>
2023-04-21 13:53:55 -0400

    Hopefully seprated setch from ai command. 🤞

--- modules/AI.py
+++ modules/AI.py
@@ -6,34 +6,35 @@ class AI(commands.Cog, name="AI"):
     def __init__(self, bot):
         self.bot = bot
         self.anime_character = None
-
-    @commands.command()        
-    async def ai(self, ctx, *, prompt):
-        if prompt.lower().startswith("setch "):
-            if prompt[14:].lower() == "default":
-                self.anime_character = None
-                await ctx.send("AI model reset to default")
-                await ctx.message.add_reaction('👌')
-            else:
-                self.anime_character = prompt[14:]
-                await ctx.send(f"AI model set to: {self.anime_character}")
-                await ctx.message.add_reaction('👌')
+        
+    @commands.command()
+    async def setch(self, ctx, *, anime_character):
+        if anime_character.lower() == "default":
+            self.anime_character = None
+            await ctx.send("AI model reset to default")
+            await ctx.message.add_reaction('👌')
         else:
-            data = {
-                "model": self.get_ai_model(),
-                "prompt": prompt,
-            }
-            # Send typing indicator to indicate bot is processing request
-            async with ctx.typing():
-                try:
-                    async with aiohttp.ClientSession() as session:
-                        async with session.post('http://127.0.0.1:8080/api', json=data) as response:
-                            result = await response.json()
-                            response_chunks = [result['data'][i:i+2000] for i in range(0, len(result['data']), 2000)]
-                            for chunk in response_chunks:
-                                await ctx.send(f"```\n{chunk}\n```")
-                except Exception as e:
-                    await ctx.send(f"An error occurred while processing your request: {e}")
+            self.anime_character = anime_character
+            await ctx.send(f"AI model set to: {self.anime_character}")
+        await ctx.message.add_reaction('👌')
+    
+    @commands.command()
+    async def ai(self, ctx, *, prompt):
+        data = {
+            "model": self.get_ai_model(),
+            "prompt": prompt,
+        }
+        # Send typing indicator to indicate bot is processing request
+        async with ctx.typing():
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.post('http://127.0.0.1:8080/api', json=data) as response:
+                        result = await response.json()
+                        response_chunks = [result['data'][i:i+2000] for i in range(0, len(result['data']), 2000)]
+                        for chunk in response_chunks:
+                            await ctx.send(f"```\n{chunk}\n```")
+            except Exception as e:
+                await ctx.send(f"An error occurred while processing your request: {e}")
 
     def get_ai_model(self):
         if self.anime_character:

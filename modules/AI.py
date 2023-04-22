e09a1047c323bb57b141d42b936a82bc2ae0ca98
AugustusXVIII <61804916+Austcool-Walker@users.noreply.github.com>
2023-04-21 20:46:35 -0400

    Added gpt3-turbo!

--- modules/AI.py
+++ modules/AI.py
@@ -2,43 +2,35 @@ import discord
 from discord.ext import commands
 import aiohttp
 
-class AI(commands.Cog, name="AI"):
+class AI2(commands.Cog, name="AI2"):
     def __init__(self, bot):
         self.bot = bot
-        self.anime_character = None
+        self.prompt_history = {}
 
     @commands.command()        
-    async def ai(self, ctx, *, prompt):
-        if prompt.lower().startswith("setch "):
-            self.anime_character = prompt[6:].strip()
-            await ctx.send(f"AI model set to: {self.anime_character}")
-            await ctx.message.add_reaction('👌')
-        elif prompt.lower() == "setch":
-            await ctx.send("Please provide an anime character after `setch`.")
-        elif prompt.lower() == "reset":
-            self.anime_character = None
-            await ctx.send("Session state of the AI model has been reset.")
-            await ctx.message.add_reaction('👌')
-        else:
-            data = {
-                "model": self.get_ai_model(),
-                "prompt": prompt,
-            }
-            # Send typing indicator to indicate bot is processing request
-            async with ctx.typing():
-                try:
-                    async with aiohttp.ClientSession() as session:
-                        async with session.post('http://127.0.0.1:8080/api', json=data) as response:
-                            result = await response.json()
-                            await ctx.send(f"```\n{result['data'].strip()}\n```")
-                except Exception as e:
-                    await ctx.send(f"An error occurred while processing your request: {e}")
+    async def ai2(self, ctx, *, prompt):
+        user_id = ctx.author.id
+        if user_id in self.prompt_history:
+            prompt = self.prompt_history[user_id] + " " + prompt
 
-    def get_ai_model(self):
-        if self.anime_character:
-            return f"openai:text-davinci-002"
-        else:
-            return "openai:text-davinci-002"
+        data = {
+            "model": "openai:gpt-3.5-turbo",
+            "prompt": prompt
+        }
+
+        async with ctx.typing():
+            try:
+                async with aiohttp.ClientSession() as session:
+                    async with session.post('http://127.0.0.1:8080/api', json=data) as response:
+                        result = await response.json()
+                        output = result['data']
+                        if 'prompt' in result:
+                            prompt = result['prompt']
+                            self.prompt_history[user_id] = prompt
+                        for chunk in [output[i:i+2000] for i in range(0, len(output), 2000)]:
+                            await ctx.send(f"```\n{chunk}\n```")
+            except Exception as e:
+                await ctx.send(f"An error occurred while processing your request: {e}")
 
 def setup(bot):
-    bot.add_cog(AI(bot))
+    bot.add_cog(AI2(bot))

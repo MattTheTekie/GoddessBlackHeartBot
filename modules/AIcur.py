ad6479a1a7aafe4767d2710d818bf246872b0ca9
AugustusXVIII <61804916+Austcool-Walker@users.noreply.github.com>
2023-05-31 21:02:55 -0400

    Add files via upload

---
+++ modules/AIcur.py
@@ -0,0 +1,52 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import json
+from datetime import date, time, datetime, timedelta
+import aiohttp
+import random
+import requests
+import asyncio
+import subprocess
+import openai
+
+class AIcur(commands.Cog, name="AIcur"):
+    def __init__(self, bot):
+        self.bot = bot
+        self.openai_token = "pk-lqRPVysXvAPeooisGFSZkNLzVGamczCHbarsOnAoEVzlhpPt"
+
+    @commands.command()
+    async def aicur(self, ctx, *, prompt: str):
+        headers = {
+            'Authorization': f'Bearer {self.openai_token}',
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'model': 'text-davinci-003',
+            'prompt': f'"{prompt}"\nAI:',
+            'temperature': 0.7,
+            'max_tokens': 256,
+            'stop': ['Human:', 'AI:']
+        }
+
+        async with aiohttp.ClientSession() as session:
+            user_input = await self.get_user_input(ctx)
+            data['prompt'] = f'{prompt}\nUser: {user_input}\nAI:'
+            
+            async with ctx.typing():
+                async with session.post('https://api.pawan.krd/v1/completions', headers=headers, json=data) as response:
+                    if response.status == 200:
+                        result = await response.json()
+                        text = result['choices'][0]['text']
+                        await ctx.send(f"```\n{text}\n```")
+                    else:
+                        await ctx.send(f"Command failed with status code {response.status}")
+
+    async def get_user_input(self, ctx):
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+        user_input_msg = await self.bot.wait_for('message', check=check)
+        return user_input_msg.content
+
+def setup(bot):
+    bot.add_cog(AIcur(bot))
